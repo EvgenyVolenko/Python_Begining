9584a2c1394967ad2934c76e047cc880f82b99d3
Evgeny <evolenko@inbox.ru>
2023-02-06 13:39:51 +0300

    HW_2 with 2 hard

--- Seminars/HW_2/Task_004_optional.py
+++ Seminars/HW_2/Task_004_optional.py
@@ -3,20 +3,37 @@
 # *Пример:*
 # - для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
 
+def febon (k):
+    if k >= 0: 
+        f3 = 0
+    if k >= 1:
+        f3 = 1
+    if k >= 2:
+        f1 = 0
+        f2 = 1
+        f3 = f1 + f2
+        i = 2
+        while i < k:
+            temp = f3
+            f3 = f3 + f2
+            f2 = temp
+            i += 1   
+    return f3
+
 try:
-    digit = int(input('Введите порядковый номер числа Фибоначчи: '))
+    digit = int(input('Введите значение k для расчета чисел Фибоначчи: '))
+    digitOtr = - digit
+    
+    arrayF = []
 
-    f1 = 0
-    f2 = 1
-    f3 = f1 + f2
-    i = 2
-
-    while i < digit:
-        temp = f3
-        f3 = f3 + f2
-        f2 = temp
-        i += 1   
-    print(f"Порядковый номер числа Фебоначчи {digit}, а число {f3}")
+    if digitOtr < 0:
+        for i in range(digitOtr, 0):
+            arrayF.append((-1)**(abs(i) - 1) * febon(abs(i)))
+    if digit >= 0:
+        for i in range(0, digit + 1):
+            arrayF.append(febon(i))
+    
+    print(f"Значение k {digit}, а числа {arrayF}")
 
 except:
     print("Вы ввели некоректные данные")
--- Seminars/HW_2/Task_005_optional.py
+++ Seminars/HW_2/Task_005_optional.py
@@ -1,3 +1,33 @@
 # Задача 5 - HARD необязательная
 # Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними
 # в N-мерном пространстве. Сначала задается N с клавиатуры, потом задаются координаты точек.
+
+from random import randint
+
+def dist(coordt1, coordt2):
+    d2 = 0
+    for i in range(len(coordt1)):
+        d2 += (coordt1[i] - coordt2[i]) ** (2)
+    dist = d2 ** (0.5)
+    return dist
+
+def coords (N, min, max):
+    arrayA = []
+    for i in range(N):
+        arrayA.append(randint(min, max))
+    return arrayA
+
+try:
+    nMer = int(input('Введите мерность пространства: '))
+    min = int(input('Введите начало диапазона координат: '))
+    max = int(input('Введите конец диапазона координат: '))
+      
+    arrayt1 = coords(nMer, min, max)
+    arrayt2 = coords(nMer, min, max)
+
+    D = round(dist(arrayt1, arrayt2), 2)
+
+    print(f"Расстояние между точками = {D}")
+            
+except:
+    print("Вы ввели некоректные данные")
