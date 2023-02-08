3aac291d481ac914669e5a384a093367fc43a537
Evgeny <evolenko@inbox.ru>
2023-02-08 23:26:25 +0300

    HW_4 with 1  hard

--- Seminars/HW_4/Task_RLE_HW.py
+++ Seminars/HW_4/Task_RLE_HW.py
@@ -9,23 +9,43 @@ def coding(strNot):
     for i in strNot:
         strSpis.append(i)
 
-    print(strSpis)
-    count = 1
-    fSimb = strSpis[0]
-    for i in range(1, len(strSpis)):
-        if fSimb == strSpis[i]:
-            count += 1
-            print(strSpis[i], count)
-        else:
-            if count > 1:
-                strCount.append(count)
-            strCount.append(fSimb)
-            fSimb = strSpis[i]
+    count = 0
+    fSimb = ''
+    for i in range(len(strSpis)):
+        if fSimb != strSpis[i]:
+            if fSimb != None:
+                if count > 1:
+                    strCount.append(str(count))
+                strCount.append(fSimb)
             count = 1
-            print(strSpis[i], count)
-    return strCount
+            fSimb = strSpis[i]
+        else:
+            count += 1
+    else:
+        if count > 1:
+            strCount.append(str(count))
+        strCount.append(fSimb)
+    strNot = ''.join(strCount)
+    return strNot
 
-str = 'aaabccccCCaB'
-print(str)
-str = coding(str)
-print(str)
+def decoding(strYes):
+    count = ''
+    strOut = ''
+    for simbol in strYes:
+        if simbol.isdigit():
+            count += simbol
+        else:
+            if not count:
+                strOut += simbol
+            else:
+                for i in range(int(count)):
+                    strOut += simbol
+            count = ''
+    return strOut
+
+strS = 'aaabccccCCaBBd'
+print(strS)
+strS = coding(strS)
+print(strS)
+strS = decoding(strS)
+print(strS)
